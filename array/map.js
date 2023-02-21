16d1df166e1d383c252cb6bce0aa11730b90751c
sidneysmmbh <sidneysmmbh@hotmail.com>
2023-02-21 11:24:31 -0300

    Refatorando o código no arquivo map.js

--- array/map.js
+++ array/map.js
@@ -1,13 +1,15 @@
 const alunos = [
-  { nome: "Pedro", nota1: 15, nota2: 30, nota3: 27 },
+  { nome: "Pedro", nota1: 15, nota2: 31, nota3: 27 },
   { nome: "Renata", nota1: 15, nota2: 25, nota3: 36 },
+  { nome: "Laura", nota1: 17, nota2: 28, nota3: 27 },
 ];
 
-const alunosNotaTotal = alunos.map(aluno => retornarNomeNotaTotal(aluno));
+const alunosNotaTotal = alunos.map(retornarNomeNotaTotal);
 
-function retornarNomeNotaTotal({ nome, nota1, nota2, nota3 }) {
-  const total = nota1 + nota2 + nota3;
-  return {nome:nome,notaTotal:total}
+function retornarNomeNotaTotal({ nome, nota1, nota2, nota3 }=aluno, index) {
+  const notaTotal = nota1 + nota2 + nota3;
+  const numeroAluno = index+1
+  return {numeroAluno,nome,notaTotal}
 }
 
 console.log(alunos)
